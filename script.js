2d2a6018fb9730c4cd1182773e85d612c5205beb
Joana Grave <joana_grave@hotmail.com>
2023-01-15 23:33:42 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function printMessage(element){
+  console.log("element");
+  element.innerText = "Logout";
+}
+
+
+function ninjalike(){
+  alert("Ninja was liked");
+}
+
+function turkey(){
+  var portugal = document.querySelector("#portugal");
+  portugal.remove()
+}
